ccd84c552c1fb24d3ec65d356a995add94fdea06
Gonéri Le Bouder <goneri.lebouder@enovance.com>
2014-06-19 13:10:24 +0200

    create an initial Jenkins job
    this Jenkins job pull git://$upstream_ip/git/demo.git where $upstream_ip
    is either 127.0.0.1 (the default), or a value passed using --extra_params
    
    Change-Id: I2ce8a324dcc0a6372dd4d7b03c3da78ca24b7e0d

--- samples/jenkins/configure_jenkins.sh
+++ samples/jenkins/configure_jenkins.sh
@@ -1,10 +1,57 @@
 #!/bin/bash
 
+if [ -z $upstream_git ]; then
+    upstream_git="/git/input.git"
+    mkdir -p /git/input.git
+    git init --bare --shared /git/input.git
+    chown -R stack:stack /git/input.git
+    git clone /git/input.git /tmp/input
+    cd /tmp/input
+    cp /etc/fstab /tmp/input
+    git add fstab
+    git commit -m "initial commit"
+    git push /git/input.git master:master
+fi
+
+chown -R jenkins:jenkins /var/cache/git/demo.git
+
 cd /root
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar
-for plugin in git-client scm-api git; do
-    java -jar jenkins-cli.jar -s http://localhost:8080/ install-plugin /root/jenkins/plugins/${plugin}.hpi
+
+# Install all the .hpi plugin found in the /root/jenkins directory
+find /root/jenkins -name '*.hpi' -exec java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin {} \;
+
+java -jar jenkins-cli.jar -s http://localhost:8080 restart
+
+while [ $(curl  -o /dev/null -sL -w "%{http_code}\\n" http://localhost:8080/pluginManager/installed) -ne 200 ]; do
+    echo "Waiting for Jenkins"
+    sleep 1
 done
 
-java -jar jenkins-cli.jar -s http://localhost:8080/ safe-restart
+cat > /tmp/jenkins_job.yaml <<EOF
+- job:
+    name: 'validate-kitchen'
+    description: 'Fetch a remote marmite and test it'
+    project-type: freestyle
+    wrappers:
+      - ansicolor:
+          colormap: xterm
+    scm:
+      - git:
+          url: $upstream_git
+          refsepc: 'master'
+          wipe-workspace: true
+    triggers:
+      - timed: '@daily'
+      - pollscm: "* * * * *"
+    builders:
+      - shell: |
+          set -e
+          echo "this is the end"
+          git checkout -b master `git log --pretty=%H`
+          git push /var/cache/git/demo.git master:master
+EOF
+jenkins-jobs update /tmp/jenkins_job.yaml
+
+
 echo "Jenkins is ready!"
