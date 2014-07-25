8eff1b9a9a17199b00570c09b70c444735707ee1
Chmouel Boudjnah <chmouel.boudjnah@enovance.com>
2014-07-25 16:56:37 +0200

    Add run_tests file
    Allow to switch between test7 and test6
    This is used by the CI but can be used for the end user as well
    
    Change-Id: I93f8db54f9766a0c96435a8092e844d3c849b603

---
+++ run_tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+TARGET=${1:-test7}
+
+set -ex
+set -o pipefail
+
+function test7 () {
+    export OS_IDENTITY_API_VERSION=2.0
+    export OS_PASSWORD=password
+    export OS_AUTH_URL=http://10.151.68.51:5000/v2.0
+    export OS_USERNAME=admin
+    export OS_TENANT_NAME=demo
+    export OS_VOLUME_API_VERSION=2
+    export OS_NO_CACHE=1
+}
+
+function test6 () {
+    export OS_IDENTITY_API_VERSION=2.0
+    export OS_PASSWORD=password
+    export OS_AUTH_URL=http://10.151.68.50:5000/v2.0
+    export OS_USERNAME=admin
+    export OS_TENANT_NAME=demo
+    export OS_VOLUME_API_VERSION=2
+    export OS_NO_CACHE=1
+}
+
+
+# we can be clever when it get complicated
+if [[ ${TARGET} == "test6" ]];then
+    test6
+elif [[ ${TARGET} == "test7" ]];then
+    test7
+fi
+
+kitchen-mincer --target devtest samples/jenkins
+
+exit $?
