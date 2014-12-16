goneri/kitchen-mincer

commit 5e9fbc96ec6a831b0805450a833dd57786df369b

heat: correctly initialize the volume_id parameter
Change-Id: I2c1b8ddaf8f9c05cc09e8a9632ee42580a450de7

mincer/providers/heat/provider.py
@@ -259,7 +259,7 @@ resources:
         """
         medias_to_upload = self._filter_medias(medias, refresh_medias)
         self._upload_medias(medias_to_upload)
-        return self._wait_for_medias_in_glance(medias_to_upload)
+        self._wait_for_medias_in_glance(medias_to_upload)
 
     def _show_media_upload_status(self, name, fd, size):
         time.sleep(5)
@@ -307,20 +307,19 @@ resources:
     def _wait_for_medias_in_glance(self, medias_to_upload):
         parameters = {}
         LOG.info("Checking the image(s) status")
-        while len(parameters) != len(medias_to_upload):
+        while len(self.medias) != len(medias_to_upload):
             for local_media in medias_to_upload.values():
                 image = self._glance.images.get(local_media.glance_id)
                 LOG.debug("status: %s - %s", image.name, image.status)
                 if image.status == 'active':
                     LOG.info("Image %s is ready" % image.name)
                     local_media_id = local_media.glance_id
-                    parameters['volume_id_%s' % image.name] = local_media_id
+                    self.medias['volume_id_%s' % image.name] = local_media_id
                 elif image.status == 'killed':
                     raise ImageException("Error while waiting for image")
                 else:
                     LOG.info("waiting for %s", local_media.name)
                     time.sleep(5)
-        return parameters
 
     def _register_pub_key(self, test_public_key):
         """Register the public key in the provider

commit 69753062eb204ccc3ee6f8cbf3496f546c7817d9

fix medias upload test coverage
unit-tests were broken since 5e9fbc96ec6a831b0805450a833dd57786df369b

Change-Id: I00183d0eaab67e62291a97d27209c8e68f7f1a12

mincer/providers/heat/provider.py
@@ -305,12 +305,10 @@ resources:
                                 local_media.size)
 
     def _wait_for_medias_in_glance(self, medias_to_upload):
-        parameters = {}
         LOG.info("Checking the image(s) status")
         while len(self.medias) != len(medias_to_upload):
             for local_media in medias_to_upload.values():
                 image = self._glance.images.get(local_media.glance_id)
-                LOG.debug("status: %s - %s", image.name, image.status)
                 if image.status == 'active':
                     LOG.info("Image %s is ready" % image.name)
                     local_media_id = local_media.glance_id

mincer/providers/heat/tests/test_provider.py
@@ -275,16 +275,15 @@ class TestProvider(testtools.TestCase):
         my_provider._glance.images.get.return_value = mock_image_1
 
         with mock.patch('%s.open' % six.moves.builtins.__name__):
-            actual_parameters = my_provider.upload(medias, ["name_1"])
-        self.assertEqual(actual_parameters.keys(),
+            my_provider.upload(medias, ["name_1"])
+        self.assertEqual(my_provider.medias.keys(),
                          {'volume_id_name_1': None}.keys())  # Py34
 
+        my_provider.medias = {}
         my_provider._glance.images.get.return_value = mock_image_3
         medias = {"name_1": mock_image_1}
         self.assertRaises(provider.ImageException,
                           my_provider.upload, medias, ["name_1"])
-        self.assertEqual(actual_parameters.keys(),
-                         {'volume_id_name_1': None}.keys())
 
     def _create_images_in_glance(self):
         img_in_glance = mock.Mock()
@@ -436,15 +435,19 @@ class TestProvider(testtools.TestCase):
         my_media.glance_id = 123
         my_provider._glance = mock.Mock()
         my_provider._glance.images.get.return_value = my_media
-        self.assertEqual(my_provider._wait_for_medias_in_glance({}), {})
+        my_provider._wait_for_medias_in_glance({})
+        self.assertEqual(my_provider.medias, {})
 
+        my_provider.medias = {}
         provider.LOG = mock.Mock()
         my_media.status = 'active'
         my_provider._wait_for_medias_in_glance({'bob': my_media})
         provider.LOG.info.assert_called_with(
             'Image Kim is ready')
 
+        my_provider.medias = {}
         my_media.status = 'killed'
+        my_provider._glance.images.get.return_value = my_media
         self.assertRaises(provider.ImageException,
                           my_provider._wait_for_medias_in_glance,
                           {'bob': my_media})